3d6960e4a1742f481d43748529dd3f580df1e06d
hungaria <hungaria@student.21-school.ru>
2024-01-22 18:04:27 +0300

    r2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Box, Button, Container, IconButton, Toolbar, Typography } from '@mui/material';
+import {AppBar, Box, Button, Container, IconButton, Paper, Grid, Toolbar, Typography} from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useTheme } from '@mui/material/styles';
@@ -13,27 +13,50 @@ const useStyles = makeStyles(() => ({
   },
   title: {
     flexGrow: 1
+  },
+  currencyTable: {
+    position: "relative",
+    color: useTheme().palette.common.white,
+    marginBottom: useTheme().spacing(4),
   }
 }));
+
 
 function App() {
   const classes = useStyles();
 
   return (
-        <AppBar position="fixed">
-          <Container fixed>
-            <Toolbar>
-              <IconButton edge="start" color="inherit" area-label="menu" className={classes.menuButton}>
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h5" className={classes.title}>Обменка</Typography>
-              <Box  mr={3}>
-                <Button color="inherit" variant="outlined">Log In</Button>
-              </Box>
-              <Button color="secondary" variant="contained">Sign Up</Button>
-            </Toolbar>
-          </Container>
-        </AppBar>
+      <>
+    <AppBar position="fixed">
+      <Container fixed>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" area-label="menu" className={classes.menuButton}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h5" className={classes.title}>Обменка</Typography>
+          <Box  mr={3}>
+            <Button color="inherit" variant="outlined">Log In</Button>
+          </Box>
+          <Button color="secondary" variant="contained">Sign Up</Button>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    <main>
+      <Paper className={classes.currencyTable}>
+        <Container fixed>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Paper>12</Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper>6</Paper>
+            </Grid>
+          </Grid>
+        </Container>
+      </Paper>
+    </main>
+
+< />
   );
 }
 
