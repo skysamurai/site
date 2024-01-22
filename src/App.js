e6e7943733f96aaa1a62035197b478b8c23707cd
hungaria <hungaria@student.21-school.ru>
2024-01-22 22:57:13 +0300

    add currencyInput

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import {AppBar, Box, Button, Container, IconButton, Paper, Grid, Toolbar, Typography} from '@mui/material';
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Paper,
+  Grid,
+  Toolbar,
+  Typography,
+  MenuItem,
+  TextField, Select, FormControl, InputLabel
+} from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useTheme } from '@mui/material/styles';
+
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -15,10 +28,23 @@ const useStyles = makeStyles(() => ({
     flexGrow: 1
   },
   currencyTable: {
-    position: "relative",
-    color: useTheme().palette.common.white,
-    marginBottom: useTheme().spacing(4),
-  }
+    marginTop: useTheme().spacing(14),
+  },
+  cryptoInputBox: {
+    // display: 'flex',
+    // alignItems: 'center',
+    // justifyContent: "space-between",
+  },
+  currencyInput: {
+    minWidth: '80%',
+  },
+  currencyType: {
+    minWidth: '32%',
+  },
+  // formControl: {
+  //   margin: useTheme().spacing(1),
+  //   minWidth: 120,
+  // },
 }));
 
 
@@ -28,7 +54,7 @@ function App() {
   return (
       <>
     <AppBar position="fixed">
-      <Container fixed>
+      <Container maxWidth={"lg"}>
         <Toolbar>
           <IconButton edge="start" color="inherit" area-label="menu" className={classes.menuButton}>
             <MenuIcon />
@@ -43,13 +69,42 @@ function App() {
     </AppBar>
     <main>
       <Paper className={classes.currencyTable}>
-        <Container fixed>
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <Paper>12</Paper>
+        <Container maxWidth={"lg"}>
+          <Grid container spacing={3} columns={14}>
+            <Grid item xs={10}>
+              <Paper>10</Paper>
             </Grid>
-            <Grid item xs={6}>
-              <Paper>6</Paper>
+            <Grid item xs={4}>
+              <Paper>
+                <div className={classes.cryptoInputBox}>
+                  <FormControl sx={{mr:0.1, mt:2, minWidth: '67%' }} className={classes.currencyInput}>
+                    <TextField  label="Вы продаете"/>
+                  </FormControl>
+                  <FormControl sx={{ml:0.1, mt:2, minWidth: '32%' }} className={classes.currencyType}>
+                    <InputLabel id="demo-simple-select-autowidth-label">Валюта</InputLabel>
+                    <Select value={''} autoWidth label="Age">
+                      <MenuItem value="">Валюта</MenuItem>
+                      <MenuItem value={10}>Twenty</MenuItem>
+                      <MenuItem value={21}>Twenty one</MenuItem>
+                      <MenuItem value={22}>Twenty one and a half</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
+                <div className={classes.cryptoInputBox}>
+                  <FormControl sx={{mr:0.1, mt:2, minWidth: '67%'}} className={classes.currencyInput}>
+                    <TextField  label="Вы получаете"/>
+                  </FormControl>
+                  <FormControl sx={{ml:0.1, mt:2, minWidth: '32%'}} className={classes.currencyType}>
+                    <InputLabel id="demo-simple-select-autowidth-label">Валюта</InputLabel>
+                    <Select value={''} autoWidth label="Валюта">
+                      <MenuItem value=""><em>None</em></MenuItem>
+                      <MenuItem value={10}>Twenty</MenuItem>
+                      <MenuItem value={21}>Twenty one</MenuItem>
+                      <MenuItem value={22}>Twenty one and a half</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
+              </Paper>
             </Grid>
           </Grid>
         </Container>
